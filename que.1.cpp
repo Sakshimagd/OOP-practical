1c7677f056d7e0cfe39d507625214e5caa27d7da
Sakshimagd <magdumsakshi4578@gmail.com>
2025-01-18 23:54:29 +0530

    Add files via upload

---
+++ que.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int a = 21;
+   int b = 10;
+   int c ;
+   c = a + b;
+   cout << " Addition is:" << c << endl ;
+   
+   c = a - b;
+   cout << "substraction is :" << c << endl ;
+   
+   c = a * b;
+   cout << "multiplication is:" << c << endl ;
+   
+   c = a / b;
+   cout << "division is" << c << endl ;
+   
+   c = a % b;
+   cout << "modulus is: :" << c << endl ;
+   return 0;
+}
